41d2d589f3ed49a77afc25d3338121dd792e1cd3
IanHD04 <u202214059@upc.edu.pe>
2024-04-17 15:53:22 -0500

    chore: imported components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+//Impotaciones generales
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+
+//Los componentes que se utilizaran
+import Card from 'primevue/card'
+import Toolbar from 'primevue/toolbar';
+
+
+//Se inicializan los componentes
+createApp(App)
+    .use(PrimeVue,{ripple:true})
+    .component('pv-card',Card)
+    .component('pv-toolbar',Toolbar)
+    .mount('#app')
